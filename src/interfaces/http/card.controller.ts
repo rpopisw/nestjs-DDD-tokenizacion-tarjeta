1f93d5679537995810b0600edfb8bad885041616
Robert Popi <robertpopi@Roberts-MacBook-Air.local>
2023-03-13 15:26:51 -0500

    :school_satchel: add document

--- src/interfaces/http/card.controller.ts
+++ src/interfaces/http/card.controller.ts
@@ -1,16 +1,28 @@
 import { Body, Controller, Post, Headers, Param, Get } from "@nestjs/common";
 import { CommandBus, QueryBus } from "@nestjs/cqrs";
+import { ApiHeader, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateTokenCommand } from "src/application/commands/create-token.command";
+import { CreateTokenResponse } from "src/application/dtos/create-token-response.dto";
 import { FindCardByTokenQuery } from "src/application/queries/find-card-by-token.query";
 import { CreateTokenDto } from "./dtos/create-token.dto";
 
 @Controller('card')
+@ApiTags('card')
 export class CardController {
     constructor(
         private readonly commandBus: CommandBus,
         private readonly queryBus: QueryBus,
     ) { }
 
+    @ApiResponse({
+        status: 201,
+        description: 'The card has been successfully created and create token',
+        type: CreateTokenDto,
+    })
+    @ApiHeader({
+        name: 'pk',
+        description: 'pk business',
+      })
     @Post('create-token')
     async createToken(@Body() body: CreateTokenDto, @Headers('pk') pk: string) {
         const { card_number: cardNumber, cvv, expiration_month: expirationMonth, expiration_year: expirationYear, email } = body;
@@ -18,6 +30,14 @@ export class CardController {
         return await this.commandBus.execute(command);
     }
 
+    @ApiResponse({
+        status: 200,
+        description: 'The card has been successfully found',
+    })
+    @ApiHeader({
+        name: 'pk',
+        description: 'pk business',
+      })
     @Get(':token')
     async findCardByToken(@Param('token') token: string) {
         const query = new FindCardByTokenQuery(token);
--- src/interfaces/http/dtos/create-token.dto.ts
+++ src/interfaces/http/dtos/create-token.dto.ts
@@ -1,20 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, MaxLength, MinLength} from "class-validator";
 
 export class CreateTokenDto{
+
+    
+    @ApiProperty()
     @IsString()
     card_number: string;
 
+    @ApiProperty()
     @IsString()
     @MinLength(3)
     @MaxLength(4)
     cvv: string;
 
+    @ApiProperty()
     @IsString()
     expiration_month: string;
 
+
+    @ApiProperty()
     @IsString()
     expiration_year: string;
-    
+
+
+    @ApiProperty()
     @IsEmail()
     email: string;
 }
